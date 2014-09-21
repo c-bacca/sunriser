bb20197e48928f1e78ce73f29a6cb0629c85c1f2
c-bacca <i_never_read_my_stuff@hotmail.com>
2014-09-20 18:13:58 -0700

    Stubs for most functions.

--- sunrise.py
+++ sunrise.py
@@ -11,21 +11,47 @@ __author__ = 'cepuuher'
 
 import datetime
 
-def main ():
+def calculate_bedtime(sleep_duration, sunrise_time):
+    print 'In calculate_bedtime()...'
+    bedtime = 0
+
+    return bedtime  # stub
+
+
+def retrieve_web_sunrise_times(today, city):
+    return []  # stub... it won't be a list returned
+
+def get_sunrise(today):
+    return today  # worst stub ever
+
+def main():
     print "Welcome to sunrise. We'll calculate your bedtime based on sunrise."
 
     # Prompt for user options
-    sleep_time = input("How many hours of sleep would you like? ")
-    city = raw_input("What city are you sleeping in? ")
+    sleep_duration = input("How many hours of sleep would you like? ")
+
+#    city = raw_input("What city are you sleeping in? ")
 
     # Retrieve info
     today = datetime.date.today()
     word_date = datetime.date.ctime(today)
+
+#    print "Today is " + str(today)
+#    print "A.k.a. " + str(word_date)
+
+    # Grab sunrise info from web, if necessary
+
+    # Oh man I'm commenting too much...
+    sunrise_time = get_sunrise(today)
+
     # Calculate
-    # Return bedtime
+    bedtime = calculate_bedtime(sleep_duration, sunrise_time)
+
+    # Return bedtime to user
+    print "Tonight's bedtime is " + str(bedtime)
 
 
-if __name__ == main:
+if __name__ == main():
     try:
         main()
     except Exception as e:
